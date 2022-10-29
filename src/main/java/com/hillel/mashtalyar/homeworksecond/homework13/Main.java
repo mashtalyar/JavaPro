d445f6b5a40c4fe31d1b67e1ef38ed41639454d2
mashtalyar <111196251+mashtalyar@users.noreply.github.com>
2022-10-29 17:25:14 +0300

    Update Main.java

--- src/main/java/com/hillel/mashtalyar/homeworksecond/homework13/Main.java
+++ src/main/java/com/hillel/mashtalyar/homeworksecond/homework13/Main.java
@@ -24,10 +24,12 @@ public class Main {
         secondClass.methodFirst("Andriy");
         secondClass.methodSecond("Dmitrenco");
 
-        System.out.println(helper("serg49049@gmail.com")); //6. Вызвать в классе Main этот хелпер передав валидное значение и получить его;
+        Optional optional= (Optional) helper("serg49049@gmail.com");//6. Вызвать в классе Main этот хелпер передав валидное значение и получить его;
+        System.out.println(optional.get());
 
-        final Optional optional= (Optional) helper("132132"); //7. Вызвать в классе Main этот хелпер передав не валидное значение и вызвать ошибку используя Optional функции, пытаясь получить значение из вернувшегося результата (метод get);
-        optional.get();
+        optional= (Optional) helper("14224");//7. Вызвать в классе Main этот хелпер передав не валидное значение и вызвать ошибку используя Optional функции, пытаясь получить значение из вернувшегося результата (метод get);
+        System.out.println(optional.get());
+
     }
 }
 
